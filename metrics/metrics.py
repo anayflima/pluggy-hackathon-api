5e21799ce2909886c8e2989da1d4ede09680bb8a
anayflima <ayf.lima@usp.br>
2022-07-24 12:35:39 -0300

    Adiciona tratamento de caixa e EBITDA negativos

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -14,6 +14,15 @@ class Metrics():
         """
             Função calcula a razão entre caixa da empresa e EBITDA
             Com isso, queremos obter uma estimativa de se a empresa possui ou não receita recorrente.
+
+            Se o caixa for menor do que zero, consideraremos como zero, para que o valor negativo
+            não distorça o algoritmo (por exemplo, se caixa e EBITDA forem menores do que zero,
+            a razão dará positiva, o que criará a falsa sensação de que a empresa tem um bom caixa
+            em relação ao seu retorno e pode investi-lo em novas atividades. Faremos algo semelhante
+            com o EBITDA, mas, como este não pode ser zero pois é denominador de função, atribuiremos
+            o valor 1, de forma que a razão será igual ao caixa nesse caso.
+            Tais atribuições não serão prejudiciais ao algoritmo, já que elas continuarão
+            mais próximas de soluções que possuem baixo caixa e lucro.
         """
 
         clientTransactions = Transactions()
@@ -21,6 +30,13 @@ class Metrics():
         cash = clientTransactions.getCash(itemId)
 
         EBITDA = clientTransactions.getEBITDA(accountId)
+
+        # Remoção de valores negativos das variáveis
+        if (EBITDA <= 0):
+            EBITDA = 1
+        
+        if (cash < 0):
+            cash = 0
 
         cashOnEBITDA = cash/EBITDA
         return cashOnEBITDA
