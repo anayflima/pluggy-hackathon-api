66c208e923146e534c723eaa0ccf90e4b05ad843
anayflima <ayf.lima@usp.br>
2022-07-24 02:46:14 -0300

    Remove algumas chamadas de teste e generaliza Ids em variaveis

--- calls.py
+++ calls.py
@@ -7,52 +7,36 @@ from dataCollector import DataCollector
 sys.path.insert(0, './models/')
 from metricsClassifier import MetricsClassifier
 
-def calls():
-    clientMetrics = Metrics()
-    clientTransactions = Transactions()
-    # X_API_KEY = clientTransactions.getAPIKey("a8935a1f-a204-40de-bd86-eed00f433887", "2c61a2ee-eca2-4d69-8891-060ec0fd76cd")
-    # clientTransactions.setAPIKey(X_API_KEY)
-    totalTransations = clientTransactions.getTransactions("4d4f622a-38d6-4991-b340-387492fd22c8")
-    # print("totalTransations = " + str(totalTransations))
-    # mensalIncome = clientMetrics.getMensalIncome("4d4f622a-38d6-4991-b340-387492fd22c8")
-    # print("mensalIncome = " + str(mensalIncome))
-    # cash = clientMetrics.getCash("4d4f622a-38d6-4991-b340-387492fd22c8")
-    # print("cash = " + str(cash))
-    # cashForMensalIncome = clientMetrics.calculateCashForMensalIncome("4d4f622a-38d6-4991-b340-387492fd22c8")
-    # print("cashForMensalIncome = " + str(cashForMensalIncome))
-    # incomeVolatility = clientMetrics.getIncomeVolatility()
-    # print("incomeVolatility = " + str(incomeVolatility))
-    positiveTransactionsByMonth = clientTransactions.getPositiveTransactionsByMonth("4d4f622a-38d6-4991-b340-387492fd22c8")
-    print("positiveTransactionsByMonth = " + str(positiveTransactionsByMonth))
+accountId = "4d4f622a-38d6-4991-b340-387492fd22c8"
+itemId = "a4ac3258-e88f-4123-8dc4-a4f72c3da7a7"
+clientId = "a8935a1f-a204-40de-bd86-eed00f433887"
 
 def callsDebts():
     clientDebts = Debts()
-    # annualFinancings = clientDebts.getCurrentFinancings("a8935a1f-a204-40de-bd86-eed00f433887")
-    # print("annualFinancings = " + str(annualFinancings))
-    # annualLoans = clientDebts.getCurrentLoans("a8935a1f-a204-40de-bd86-eed00f433887")
-    # print("annualLoans = " + str(annualLoans))
-    grossDebt = clientDebts.calculateGrossDebt("a8935a1f-a204-40de-bd86-eed00f433887")
+    annualFinancings = clientDebts.getCurrentFinancings(clientId)
+    print("annualFinancings = " + str(annualFinancings))
+    annualLoans = clientDebts.getCurrentLoans(clientId)
+    print("annualLoans = " + str(annualLoans))
+    grossDebt = clientDebts.calculateGrossDebt(clientId)
     print("grossDebt = " + str(grossDebt))
-
 
 def callsMetrics():
     clientMetrics = Metrics()
-    cashForEBITDA = clientMetrics.calculateCashOnEBITDA("4d4f622a-38d6-4991-b340-387492fd22c8", "a4ac3258-e88f-4123-8dc4-a4f72c3da7a7")
+    cashForEBITDA = clientMetrics.calculateCashOnEBITDA(accountId, itemId)
     print("cashForEBITDA = " + str(cashForEBITDA))
-    incomeVolatility = clientMetrics.calculateIncomeVolatility("4d4f622a-38d6-4991-b340-387492fd22c8")
+    incomeVolatility = clientMetrics.calculateIncomeVolatility(accountId)
     print("incomeVolatility = " + str(incomeVolatility))
-    leverage = clientMetrics.calculateLeverave("a8935a1f-a204-40de-bd86-eed00f433887", "4d4f622a-38d6-4991-b340-387492fd22c8")
+    leverage = clientMetrics.calculateLeverave(clientId, accountId)
     print("leverage = " + str(leverage))
 
 def callsImpact():
     clientMetricsClassifier = MetricsClassifier()
-    cashOnEBITDAPosition = clientMetricsClassifier.calculateCashOnEBITDAPosition("4d4f622a-38d6-4991-b340-387492fd22c8", "a4ac3258-e88f-4123-8dc4-a4f72c3da7a7")
+    cashOnEBITDAPosition = clientMetricsClassifier.calculateCashOnEBITDAPosition(accountId, itemId)
     print("cashOnEBITDAPosition = " + str(cashOnEBITDAPosition))
-    incomeVolatilityPosition = clientMetricsClassifier.calculateIncomeVolatilityPosition("4d4f622a-38d6-4991-b340-387492fd22c8")
+    incomeVolatilityPosition = clientMetricsClassifier.calculateIncomeVolatilityPosition(accountId)
     print("incomeVolatilityPosition = " + str(incomeVolatilityPosition))
-    leveragePosition = clientMetricsClassifier.calculateLeveragePosition("a8935a1f-a204-40de-bd86-eed00f433887", "4d4f622a-38d6-4991-b340-387492fd22c8")
+    leveragePosition = clientMetricsClassifier.calculateLeveragePosition(clientId, accountId)
     print("leveragePosition = " + str(leveragePosition))
-    
     
 
 if __name__ == '__main__':
